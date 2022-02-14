c5fa40b2ab358a59303cd4f5a65008ad6cdb44d6
unknown <cborek@web.de>
2022-02-14 21:27:44 +0100

    Added the menu logic and styling surface

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicous Food for You!</h2>
+      <p>Choose your favorite meal now and join the worlds largest food empire today.</p>
+      <p>All our meals are just from very happy animals that has been kept in very large barracks. All bio without a trace of doubt!</p>
+    </section>
+  )
+}
+
+export default MealsSummary
